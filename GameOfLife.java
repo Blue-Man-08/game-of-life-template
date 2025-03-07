10d363bbf54c7fc8f1eced1b8b640ca8f2540696
Vedant R <110495968+Blue-Man-08@users.noreply.github.com>
2025-03-07 03:58:23 +0000

    it works!

--- GameOfLife.java
+++ GameOfLife.java
@@ -7,11 +7,12 @@ public class GameOfLife implements Board {
 
     public GameOfLife(int x, int y)
     {
-        // Construct a 2d array of the given x and y size.
+        // creates the board with (x) rows and (y) columns.
+        board = new int[x][y];
     }
 
-    // Set values on the board
     public void set(int x, int y, int[][] data) {
+        // Set values on the board
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
                 board[i + x][j + y] = data[i][j];
@@ -19,23 +20,51 @@ public class GameOfLife implements Board {
         }
     }
 
-    // Run the simulation for a number of turns
     public void run(int turns) {
-        // call step the number of times requested
+        // Run the simulation for a number of turns
+        for(int i = 0; i <= turns; i++){
+            step();
+        }
     }
 
     // Step the simulation forward one turn.
     public void step()
     {
-        print();
         // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
+        int[][] newBoard = new int[board.length][board[0].length];
+        int numNeighbors = 0;
+        for(int row = 0; row < board.length; row++){
+            for(int col = 0; col < board[row].length; col++){
+                numNeighbors = countNeighbors(row, col);
+                if(numNeighbors < 2 || numNeighbors > 3){
+                    newBoard[row][col] = 0;
+                }
+                if(numNeighbors == 3){
+                    newBoard[row][col] = 1;
+                }
+            }
+        }
+        board = newBoard;
+        print();
     }
 
 
     public int countNeighbors(int x, int y) {
-        int count = 0;
-        // count the number of neighbors the cell has
         // use the get(x,y) method to read any board state you need.
+        int count = 0;
+        int row;
+        int col;
+        for(int i = 0; i < 9; i++){
+            row = (i%3)-1;
+            col = (i/3)-1;
+            row += x;
+            col += y;
+            if(get(row,col) == 1){
+                if(row != x || col != y){
+                    count++;
+                }
+            }
+        }
         return count;
     }
 
@@ -48,14 +77,15 @@ public class GameOfLife implements Board {
         return board[(x+xLimit)%xLimit][(y+yLimit)%yLimit];
     }
 
-    // Test helper to get the whole board state
+
     public int[][] get()
     {
+        // Test helper to get the whole board state
         return board;
     }
 
-    // Test helper to print the current state
     public void print(){
+        // Test helper to print the current state
         // Print the header
         System.out.print("\n ");
         for (int y = 0; y < board[0].length; y++) {
